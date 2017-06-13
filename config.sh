22da0114969a629bb64bc443b8065e741d34e237
Val Mandantes <mandantesv@gmail.com>
2017-06-13 12:09:38 +0800

    change template to system/less

--- config.sh
+++ config.sh
@@ -2,6 +2,9 @@
 #
 # Magisk
 # by topjohnwu
+#
+# SYSTEM/LESS Installation
+# by veez21
 # 
 # This is a template zip for developers
 #
@@ -31,15 +34,25 @@ MODID=battery-charge-control
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
+# ** When system install is used, this will also be used 
+#    as a flag is if system modding will be done.
 AUTOMOUNT=true
 
 # Set to true if you need to load system.prop
+# ** When system install is used, the props in
+#    system.prop will be put directly to /system/build.prop
 PROPFILE=false
 
 # Set to true if you need post-fs-data script
+# ** When system install is used, the scripts will
+#    go to /su/su.d, /system/su.d (if SuperSU is installed
+#    or /system/etc/init.d
 POSTFSDATA=false
 
 # Set to true if you need late_start service script
+# ** When system install is used, the scripts will
+#    go to /su/su.d, /system/su.d (if SuperSU is installed
+#    or /system/etc/init.d
 LATESTARTSERVICE=false
 
 ##########################################################################################
@@ -49,9 +62,9 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Battery Charge Control    "
-  ui_print "*******************************"
+  ui_print "******************************"
+  ui_print "    Battery Charge Control    "
+  ui_print "******************************"
 }
 
 ##########################################################################################
@@ -61,6 +74,9 @@ print_modname() {
 # List all directories you want to directly replace in the system
 # By default Magisk will merge your files with the original system
 # Directories listed here however, will be directly mounted to the correspond directory in the system
+# ** When system install is used, the directories listed will be renamed to 'previous-foldername.bak'.
+#    Example: /system/app/Youtube -> /system/app/Youtube.bak
+#    If PERMANENTDELETE variable is set to true, it will permanently delete the folder from /system.
 
 # You don't need to remove the example below, these values will be overwritten by your own list
 # This is an example
@@ -75,6 +91,26 @@ REPLACE="
 # !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
 "
+
+##########################################################################################
+# Install Configuration
+##########################################################################################
+
+# You can tweak your installation process by putting Variables in /dev/.config
+# Valid Variables:
+#   MAGISKINSTALL - forces magisk installation (might conflict with SYSTEMINSTALL) (values: true or false)
+#   SYSTEMINSTALL - forces system installation (might conflict with MAGISKINSTALL) (values: true or false)
+#   INITPATH - sets path to install scripts (post-fs-data.sh, service.sh) if system install (values: directories)
+#   BUILDPROP - sets properties from system.prop directly to build.prop (values: true or false)
+#   PERMANENTDELETE - PERMANENTLY delete folders in $REPLACE (values: true or false)
+# Editing here directly is also valid, but will be overwritten in /dev/.config
+# Leave it blank if you don't need it.
+# MAGISKINSTALL is enabled by default to prioritize Magisk.
+MAGISKINSTALL=true
+SYSTEMINSTALL=
+INITPATH=
+BUILDPROP=
+PERMANENTDELETE=
 
 ##########################################################################################
 # Permissions
@@ -98,5 +134,17 @@ set_permissions() {
   # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
   # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
   # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
-  set_perm $MODPATH/system/bin/bcc_magisk 0 0 0755 0644
+  set_perm $MODPATH/system/bin/bcc_magisk 0 0 0755
+}
+
+set_permissions_system() {
+  # THIS WILL ONLY GET INVOKED WHEN SYSTEM INSTALL IS USED
+  # USE THIS TO SET PERMISSIONS TO SYSTEM FILES
+  # SEE set_permissions for instructions.
+
+  # Default permissions, don't remove them
+  set_perm  /system/build.prop  0  0  0644
+  
+  # Add commands below
+  set_perm /system/bin/bcc_magisk 0 0 0755
 }
